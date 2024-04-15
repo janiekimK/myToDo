982d58de71cdad14d0f5d666154f743d7c0a6b68
janie <janie.kabashi@bbzbl-it.ch>
2024-04-15 15:50:47 +0200

    Initial commit

---
+++ src/main/java/ch/kabashi/janie/MyToDo/MyToDoApplication.java
@@ -0,0 +1,13 @@
+package ch.kabashi.janie.MyToDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyToDoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/kabashi/janie/MyToDo/MyToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.kabashi.janie.MyToDo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
