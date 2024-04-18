ac1eb602f76366c3d9379ac9ba5700f71a61a243
janie <janie.kabashi@bbzbl-it.ch>
2024-04-18 09:17:00 +0200

    added dependencies

--- src/main/java/ch/kabashi/janie/MyToDo/base/MessageResponse.java
+++ src/main/java/ch/kabashi/janie/MyToDo/base/MessageResponse.java
@@ -1,9 +1,16 @@
 package ch.kabashi.janie.MyToDo.base;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+
 public class MessageResponse {
     private String message;
 }
+
